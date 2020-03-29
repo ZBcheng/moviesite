2643369e6124ad8ac4d597c258d9349009e5d482
bee <Bee0_0@icloud.com>
2020-03-30 00:17:05 +0800

    edit detail

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import UserView, LoginView
+from .views import UserUpdateView
 
 urlpatterns = [
     path('', UserView.as_view()),
-    path('login', LoginView.as_view())
+    path('login', LoginView.as_view()),
+    path('update', UserUpdateView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+import os
 import json
+import base64
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
@@ -7,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
 
+from moviesite.settings import MEDIA_URL
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 # Create your views here.
@@ -81,3 +84,70 @@ class UserView(generics.ListAPIView):
         username = self.request.query_params.get('username')
         user = UserProfile.objects.filter(username=username)
         return user
+
+
+class UserUpdateView(APIView):
+    '''update the view of a specific user'''
+
+    def put(self, request):
+        request_body = json.loads(request.body, encoding='utf-8')['data']
+        username = request_body['username']
+        user = UserProfile.objects.get(username=username)
+
+        # optional keys
+        try:
+            phone = request_body['phone']
+        except KeyError:
+            phone = user.phone
+        try:
+            email = request_body['email']
+        except KeyError:
+            email = user.email
+
+        avatar_name = request_body['avatar_name']  # the name of the image
+        avatar_url = request_body['avatar_url']  # the url of the image
+
+        if avatar_url.startswith('http'):
+            # that means user didn't change his/her avatar
+            pass
+        else:
+            # in this situation, avatar_url is a str object encoded with base64
+            real_url = get_real_url(avatar_url)
+            save_as_img(avatar_name, real_url)
+            user.avatar = 'avatar/' + avatar_name  # save path
+
+        user.phone = phone
+        user.email = email
+        user.save()
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
+
+
+def get_real_url(raw_url: str) -> str:
+    '''
+    remove trush data in raw_url
+    Args:
+        raw_url: raw url get from frontend
+    '''
+
+    return raw_url.split(',')[1]
+
+
+def save_as_img(img_name: str, b64_url: str):
+    '''
+    convert image url to jpg
+    Args:
+        img_name: the name of the jpg file
+        b64_url: a str object encoded with base64
+    '''
+
+    bin_url = base64.b64decode(b64_url)
+    current_path = os.path.abspath(__file__)
+    project_path = os.path.abspath(
+        os.path.dirname(current_path) + os.path.sep + "..")  # the path of the django project
+    file_path = project_path + MEDIA_URL + 'avatar/' + img_name
+
+    with open(file_path, 'wb') as file:
+        file.write(bin_url)
+
+    return True
